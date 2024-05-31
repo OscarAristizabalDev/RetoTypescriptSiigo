ae02155c88917d3e8fd56e3e3b170d8740d4d640
ClaudiaEB <claudia.espino.becerril@gmail.com>
2024-05-31 11:23:45 -0600

    Recuperación de información y presentación de la misma
    Se recuperó la información almacenada previamente en el LocalStorage  para presentarla visualmente, tanto en la consola como en la página principal

--- app.ts
+++ app.ts
@@ -1,23 +1,26 @@
-
 import { localStorageEnum } from './enums/enum';
 import { Character } from './interfaces/interfaces';
 import { getCharactersByPage } from "./services/CharacterServices";
 
 
 export let characters: Character[] = []
+export let arrCaracteres: Array<Character> = [];  
 
 export const getCharactersByNumber = async () => {
+    arrCaracteres.length=0;
+    document.getElementById('lista')!.innerHTML='';
 
     const pageCharacter = document.getElementById("pageCharacter") as HTMLInputElement;
 
     if(isNaN(parseInt(pageCharacter.value))) return;
 
     const { results } = await getCharactersByPage(parseInt(pageCharacter.value));
-    setCharacters(results)
+    setCharacters(results);
+    showCharacters();
 }
 
 const setCharacters = (characters: Character[]) => {
-    localStorage.setItem(localStorageEnum.key, JSON.stringify(characters));
+    localStorage.setItem(localStorageEnum.key, JSON.stringify(characters))
 }
 
 const displayCharacters = (characters: Character[]) => {
@@ -33,3 +36,23 @@ const displayCharacters = (characters: Character[]) => {
         }
     }
 }
+
+export const showCharacters = () => {
+  
+        if (localStorage.getItem(localStorageEnum.key)) {        
+          arrCaracteres=JSON.parse(localStorage.getItem(localStorageEnum.key)!)    
+                
+          if (arrCaracteres.length>0)      
+          {                    
+             arrCaracteres.forEach(element => {
+                console.log(element);
+                document.getElementById('lista')!.innerHTML= document.getElementById('lista')!.innerHTML + element.id + '   ' 
+                 + element.name + '   ' +  element.status + '   ' + element.species + '   ' + element.type + '<br>';
+    
+    
+             });
+          
+          }
+        
+        }     
+    }
